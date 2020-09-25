8d552d8d8f69ce23116eeb0899e837f6fce64106
pemasherpa <pemawang4@gmail.com>
2020-09-25 16:25:25 -0500

    Frist commit

---
+++ sherpaacademy/src/main/java/com/pema/expensetracker/SherpaacademyApplication.java
@@ -0,0 +1,13 @@
+package com.pema.expensetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SherpaacademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SherpaacademyApplication.class, args);
+	}
+
+}
---
+++ sherpaacademy/src/test/java/com/pema/expensetracker/SherpaacademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pema.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SherpaacademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
